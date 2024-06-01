265ced632397bc83a011d8a9b564ce71a1175519
Steve Dao <daoduyquang91@gmail.com>
2024-06-01 19:26:11 +0000

    doing

--- my_app/src/App.tsx
+++ my_app/src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@mui/material' 
 
 function App() {
-  const list = ['steve']
+  const [text, setText] = useState(0)
+  const User = createContext('')
   return (
     <div className="App">
-      <Button 
-        variant="outlined" 
-        onClick={() => {alert('Hi')}}>
-          Click
-      </Button>
+      <input type='range' min={0} max={100} onChange={e => setText(e.target.valueAsNumber)} />
+      <Form text={text}>Hi</Form>
     </div>
   );
+}
+
+function Form({ text }) {
+  return (
+    <div>
+      <p> Hi {text} </p>
+    </div>
+  )
 }
 
 export default App;
