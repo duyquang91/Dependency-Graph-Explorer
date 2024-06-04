8f1ad436a140a18d2a4bb5da7cf49f84e2f6f346
Steve Dao <daoduyquang91@gmail.com>
2024-06-04 18:33:32 +0000

    validate file

--- src/Import.tsx
+++ src/Import.tsx
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom'
 function Import() {
     const navigate = useNavigate()
     const [text, setText] = useState('')
+    const [error, setError] = useState(false)
     var selectedIndex = 0
     const [provider, setProvider] = useState(dependencyManagerProviders[0])
     const isMobile: Boolean = useContext(IsMobileContext)
@@ -16,7 +17,12 @@ function Import() {
     }
     const startParsing = (e: React.MouseEvent) => {
         provider.updateResolvedFile(text)
-        navigate('/graphViewer/0')
+        if (provider.isValid) {
+            setError(false)
+            navigate(`/graphViewer/${selectedIndex}`)
+        } else {
+            setError(true)
+        }
     }
 
     return (
@@ -37,7 +43,7 @@ function Import() {
                             <TextField variant='outlined' defaultValue={provider.name} select label='Dependency manager:'>
                                 {dependencyManagerProviders.map((item, index) => <MenuItem key={item.name} value={item.name} onClick={(e) => {selectIndex(e, index)}}>{item.name} </MenuItem>)}
                             </TextField>
-                            <TextField variant='outlined' multiline fullWidth label={provider.resolvedFileName} rows={8} value={text} onChange={(e) => {setText(e.target.value)}} />
+                            <TextField error={error} variant='outlined' multiline fullWidth label={error ? 'Invalid file format!' : provider.resolvedFileName} rows={8} value={text} onChange={(e) => {setText(e.target.value); setError(false)}} />
                             <Button variant='contained' disabled={text === ''} onClick={(e) => {startParsing(e)}}>Start</Button>
                             <Divider>or</Divider>
                             <Button variant='outlined' onClick={(e) => navigate('/graphViewer/-1')}>Try a demo</Button>
