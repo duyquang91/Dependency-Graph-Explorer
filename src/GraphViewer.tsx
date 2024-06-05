347c686c75305ab40f60837a1e343fd883d8181b
Steve Dao <daoduyquang91@gmail.com>
2024-06-05 10:56:51 +0000

    doing

--- src/GraphViewer.tsx
+++ src/GraphViewer.tsx
@@ -1,14 +1,12 @@
-import React, { ChangeEvent, ChangeEventHandler, useContext, useEffect, useRef, useState } from "react"
-import { GraphCanvas, GraphCanvasRef, darkTheme, lightTheme, useSelection } from "reagraph"
-import { DependencyProviderInterface, getSubgraph } from "./DependencyManagerProviders/DependencyProvider"
+import React, { useContext, useEffect, useRef, useState } from "react"
+import { GraphCanvas, GraphCanvasRef, darkTheme, lightTheme, useSelection, InternalGraphNode, CollapseProps } from "reagraph"
+import { getSubgraph } from "./DependencyManagerProviders/DependencyProvider"
 import { useParams } from "react-router-dom"
 import { dependencyManagerProviders } from "./DependencyManagerProviders/DependencyManagerProviders"
-import { dark } from "@mui/material/styles/createPalette"
-import { ThemeContext } from "@emotion/react"
 import { IsDarkModeContext, IsMobileContext } from "./Base"
-import { Autocomplete, Divider, Stack, TextField } from "@mui/material"
+import { Autocomplete, Divider, ListItemIcon, ListItemText, Menu, MenuItem, MenuList, Paper, Stack, TextField, Typography } from "@mui/material"
 import { CocoaPodsProvider } from "./DependencyManagerProviders/CocoaPodsProvider"
-import * as fs from 'fs';
+import { ThreeEvent } from "@react-three/fiber"
 
 function MockProvider(): CocoaPodsProvider {
   const pod = new CocoaPodsProvider()
@@ -26,7 +24,7 @@ function GraphViewer() {
   const [nodes, setNodes] = useState(provider.graph!.nodes)
   const [edges, setEdges] = useState(provider.graph!.edges)
   const rootNodeChanged = (value: string) => {
-    if (value === '') { return } 
+    if (value === '') { return }
     let graph = getSubgraph(value!, provider.graph!)
     setNodes(graph.nodes)
     setEdges(graph.edges)
@@ -61,36 +59,43 @@ function GraphViewer() {
 
   return (
     <div>
-      <div style={{
-        zIndex: 9,
-        position: 'absolute',
-        left: 15,
-        top: 15,
-        width: `${ isMobile ? '90%' : '40%' }`,
-        padding: 1,
-      }}>
-          <Autocomplete
-            options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
-            groupBy={(option) => option.firstLetter}
-            onChange={(e, v, r) => { rootNodeChanged(v?.id ?? '') }}
-            blurOnSelect
-            renderInput={(params) => <TextField {...params} label="Root node:" />}
-          />
+      <div style={{ zIndex: 9, position: 'absolute', left: 15, top: 15, width: `${isMobile ? '90%' : '40%'}`, padding: 1 }}>
+        <Autocomplete
+          options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+          groupBy={(option) => option.firstLetter}
+          onChange={(e, v, r) => { rootNodeChanged(v?.id ?? '') }}
+          blurOnSelect
+          renderInput={(params) => <TextField {...params} label="Root node:" />} />
       </div>
-      <GraphCanvas
-            ref={ref}
-            selections={selections}
-            actives={actives}
-            onNodeClick={onNodeClick}
-            onCanvasClick={onCanvasClick}
-            onNodePointerOver={onNodePointerOver}
-            onNodePointerOut={onNodePointerOut}
-            theme={isDarkMode ? darkTheme : lightTheme}
-            nodes={nodes}
-            edges={edges}
-          />
+
+      <div className="Center" style={{ zIndex: 15, position: 'absolute' }}>
+        <ContextMenu />
+      </div>
+
+      <GraphCanvas 
+        ref={ref}
+        selections={selections}
+        actives={actives}
+        onNodeClick={onNodeClick}
+        onCanvasClick={onCanvasClick}
+        onNodePointerOver={onNodePointerOver}
+        onNodePointerOut={onNodePointerOut}
+        theme={isDarkMode ? darkTheme : lightTheme}
+        nodes={nodes}
+        edges={edges} />
     </div>
   )
+
+  function ContextMenu() {
+    return (
+      <Menu open>
+      <MenuItem>Copy</MenuItem>
+      <MenuItem>Print</MenuItem>
+      <MenuItem>Highlight</MenuItem>
+      <MenuItem>Email</MenuItem>
+    </Menu>
+    )
+  }
 }
 
 
